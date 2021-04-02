1ffbc0763c94b8e7b0ba6b801f1cc2504919d73e
Thomas Buyens <thomas.buyens@student.ehb.be>
2021-04-02 22:44:37 +0200

    class Color added

--- WC_2/script.js
+++ WC_2/script.js
@@ -1,1 +1,28 @@
-console.log("test");
+'use strict'
+class Color {
+    // constructor() {
+    //     this.nR = 0;
+    //     this.nG = 0;
+    //     this.nB = 0;
+    // }
+    constructor(red, green, blue) {
+        this.nR = red;
+        this.nG = green;
+        this.nB = blue;
+    }
+
+
+     showColor() {
+        console.log(`rgb(${this.nR},${this.nG},${this.nB})`);
+        
+    }
+    colorBody() {
+        document.getElementsByTagName('body')[0].style.background = `rgb(${this.nR},${this.nG},${this.nB})`;
+    }
+
+};
+
+let a = new Color(12,123,78);
+a.showColor();
+a.colorBody();
+console.log(a.nB);
