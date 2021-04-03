84da502ebdae417516b893fb49d3096afd53b621
Thomas Buyens <thomas.buyens@student.ehb.be>
2021-04-03 11:18:43 +0200

    Oefening 4

--- WC_2/script.js
+++ WC_2/script.js
@@ -9,7 +9,7 @@ let color = {
     showColor() {
         console.log(`rgb(${this.nR},${this.nG},${this.nB})`);
     },
-
+    // OEFENING 2 uitbreiding => de if statement. Hoe van je een functieoproep met te weining argumenten op?
     setColor(red, green, blue) {
         if (red > 255 || red < 0 || green > 255 || green < 0 || blue > 255 || blue < 0) {
             console.log('Enter valid data');
@@ -20,17 +20,50 @@ let color = {
         }
     }
 }
+// OEFENING 3 uitbreiding => prompt()
+let red = prompt('Geef de roodwaarde aan.');
+let green = prompt('Geef de groenwaarde aan.');
+let blue = prompt('Geef de blauwwaarde aan.');
 
-color.showColor();
-color.setColor(12,12,12);
+
+color.setColor(red,green,blue);
 color.showColor();
 color.setColor(245,143,12);
 color.showColor();
-color.setColor(182,124);
+color.setColor(182,124, 12);
+color.showColor();
 
-// OEFENING 2 uitbreiding
+//OEFENING 4
 
 
+
+let student = {
+    name: "John Doe",
+    age: 0,
+    degree: "default",
+    courses: [],
+
+    setPersonalDetails(name, age, degree) {
+        this.name = name;
+        this.age = age;
+        this.degree = degree;
+    },
+
+    addCourse(...courses) {
+        for (const course of courses) {
+            this.courses.push(course);
+        }
+    },
+
+    showStudent() {
+        console.log( `Name: ${this.name}, age: ${this.age}, degree: ${this.degree}, courses: ${this.courses}`);
+    }
+
+}
+ //QUESTION: How to manipulate the display off arrays using literals?
+student.setPersonalDetails('Thomas', 12, 'IT');
+student.addCourse('Web dev', 'Data essentials', 'Business IT');
+student.showStudent();
 
 /*
 class Color {
