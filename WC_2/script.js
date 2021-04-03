0a44a7f5aba96484bb9ad62e290583a599909e0a
Thomas Buyens <thomas.buyens@student.ehb.be>
2021-04-03 09:57:30 +0200

    exercice 1 & 2

--- WC_2/script.js
+++ WC_2/script.js
@@ -1,4 +1,38 @@
 'use strict'
+
+// OEFENING 1
+let color = {
+    nR: 0,
+    nG: 0,
+    nB: 0,
+
+    showColor() {
+        console.log(`rgb(${this.nR},${this.nG},${this.nB})`);
+    },
+
+    setColor(red, green, blue) {
+        if (red > 255 || red < 0 || green > 255 || green < 0 || blue > 255 || blue < 0) {
+            console.log('Enter valid data');
+        } else {
+            this.nR = red;
+            this.nG = green;
+            this.nB = blue;
+        }
+    }
+}
+
+color.showColor();
+color.setColor(12,12,12);
+color.showColor();
+color.setColor(245,143,12);
+color.showColor();
+color.setColor(182,124);
+
+// OEFENING 2 uitbreiding
+
+
+
+/*
 class Color {
     // constructor() {
     //     this.nR = 0;
@@ -26,3 +60,4 @@ let a = new Color(12,123,78);
 a.showColor();
 a.colorBody();
 console.log(a.nB);
+*/
