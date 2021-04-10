8252c7427300baf570bcb3557527f1e98c729720
Thomas Buyens <thomas.buyens@student.ehb.be>
2021-04-10 20:42:31 +0200

    code along first steps

--- WC4/oefb/script.js
+++ WC4/oefb/script.js
@@ -1,5 +1,19 @@
 'use strict'
 window.onload = () => {
-    fetch('http://www.omdbapi.com/?i=tt3896198&apikey=77960cf')
+    fetch('http://www.omdbapi.com/?s=tt3896198&apikey=77960cf')
+    .then(response => console.log(response.text()))
+
+        async function getData(searchParamater) {
+            let url = await fetch(`${config.baseurl}`)
+        }
+
+    let submitForm = event => {
+        event.preventDefault();
+        
+        let value = document.getElementById('inputTitle').value;
+
+        let result = getData(value);
+    }
     
 }
+
