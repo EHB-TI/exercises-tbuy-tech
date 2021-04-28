84a8c2437597d42e67d4989df5d1e4a6d2bf3455
Thomas Buyens <thomas.buyens@student.ehb.be>
2021-04-28 10:26:49 +0200

    remove lets

--- WC6/webpack-demo/src/index.js
+++ WC6/webpack-demo/src/index.js
@@ -1,13 +1,13 @@
 import _ from 'lodash';
 
-let one = new Cleave('.input-01', {
+new Cleave('.input-01', {
   phone: true,
   phoneRegionCode: 'US',
   blocks: [6, 3, 3],
   prefix: '+32'
 });
 
-let two = new Cleave('.input-3', {
+new Cleave('.input-3', {
   date: true,
   datePattern: ['Y', 'm', 'd'],
   delimiter: '.'
