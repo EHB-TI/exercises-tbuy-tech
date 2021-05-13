51e3b76f4c673a1d58d33e2be0fb4f5e07913fd1
Thomas Buyens <thomas.buyens@student.ehb.be>
2021-05-13 09:49:39 +0200

    trying out webpack

---
+++ appliedCoding/webpack-demo/src/index.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+
+new Cleave('.form-control input-3', {
+  date: true,
+  datePattern: ['Y', 'm', 'd'],
+  delimiter: '.'
+});
+
+new Cleave('.student-name', {
+  blocks: [3, 3, 3, 2],
+  delimiters: ['.', '.', '-']
+});
