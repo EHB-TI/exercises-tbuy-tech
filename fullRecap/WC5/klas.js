b207810d795ac99cbee0e79fda32c83c47b24ff9
Thomas Buyens <thomas.buyens@student.ehb.be>
2021-06-15 14:20:51 +0200

    removed unnecessary then

--- fullRecap/WC5/klas.js
+++ fullRecap/WC5/klas.js
@@ -1,6 +1,6 @@
 'use strict'
 window.onload = () => {
-    document.querySelectorAll('.container  .dropdown  .btn')[1].innerHTML =`Leerlingen`;
+    document.querySelectorAll('.container  .dropdown  .btn')[1].innerHTML = `Leerlingen`;
     async function getData() {
         let data = await fetch("klassen.json");
         data = await data.json();
@@ -8,60 +8,57 @@ window.onload = () => {
     }
     function vuldDeCombo1() {
         getData().then(result => {
-            document.querySelectorAll('.container  .dropdown  .btn')[0].innerHTML =`Klassen`;
+            document.querySelectorAll('.container  .dropdown  .btn')[0].innerHTML = `Klassen`;
             for (const klas in result) {
 
-                document.querySelectorAll('.dropdown-menu')[0].innerHTML += `<li><a class="dropdown-item" href="#">${klas}</a></li>`;         
-            }  
-        })
-        .then(result => {
-            for (let index = 1; index < 2; index++) {
-                let stri = `#dropdownMenu${index} a`;
-                console.log(stri);
-                console.log(document.querySelectorAll(stri)[0]);
-                for (let i = 0; i < 4; i++) {
-                    console.log(document.querySelectorAll(stri)[i]);
-                    document.querySelectorAll(stri)[i].addEventListener('click', event => {
-                        vuldDeCombo2(document.querySelectorAll(stri)[i].innerHTML);
-                        console.log(document.querySelectorAll(stri)[i].innerHTML + " " + i);
-                    }); 
-                }
+                document.querySelectorAll('.dropdown-menu')[0].innerHTML += `<li><a class="dropdown-item" href="#">${klas}</a></li>`;
             }
         })
-        .then(result => {
-
-        });
+            .then(result => {
+                for (let index = 1; index < 2; index++) {
+                    let stri = `#dropdownMenu${index} a`;
+                    console.log(stri);
+                    console.log(document.querySelectorAll(stri)[0]);
+                    for (let i = 0; i < 4; i++) {
+                        console.log(document.querySelectorAll(stri)[i]);
+                        document.querySelectorAll(stri)[i].addEventListener('click', event => {
+                            vuldDeCombo2(document.querySelectorAll(stri)[i].innerHTML);
+                            console.log(document.querySelectorAll(stri)[i].innerHTML + " " + i);
+                        });
+                    }
+                }
+            });
     }
     function vuldDeCombo2(waarde) {
         let aantalItems = 0;
         getData().then(result => {
             console.log(result);
-            let i =0;
-            
+            let i = 0;
+
             for (const klas in result) {
                 if (klas == waarde) {
                     document.querySelectorAll('.dropdown-menu')[1].innerHTML = "";
                     console.log(`${result[klas] == waarde} `);
                     for (const key in result[klas]) {
-                        document.querySelectorAll('.dropdown-menu')[1].innerHTML += `<li><a class="dropdown-item" href="#">${result[klas][key]}</a></li>`;  
-                        aantalItems++;       
+                        document.querySelectorAll('.dropdown-menu')[1].innerHTML += `<li><a class="dropdown-item" href="#">${result[klas][key]}</a></li>`;
+                        aantalItems++;
                     }
                 }
                 i++;
             }
             return aantalItems
         })
-        .then(result => {
-            console.log(result);
-            for (let index = 0; index < result; index++) {
-                console.log(document.querySelectorAll('#dropdownMenu2 .dropdown-item')[index]);
-                document.querySelectorAll('#dropdownMenu2 .dropdown-item')[index].addEventListener('click', ()=> {
-                    document.getElementById('x').innerHTML = document.querySelectorAll('#dropdownMenu2 .dropdown-item')[index].innerHTML;
-                    console.log(document.querySelectorAll('#dropdownMenu2 .dropdown-item')[index].innerHTML);
-                })
-                
-            }
-        });
+            .then(result => {
+                console.log(result);
+                for (let index = 0; index < result; index++) {
+                    console.log(document.querySelectorAll('#dropdownMenu2 .dropdown-item')[index]);
+                    document.querySelectorAll('#dropdownMenu2 .dropdown-item')[index].addEventListener('click', () => {
+                        document.getElementById('x').innerHTML = document.querySelectorAll('#dropdownMenu2 .dropdown-item')[index].innerHTML;
+                        console.log(document.querySelectorAll('#dropdownMenu2 .dropdown-item')[index].innerHTML);
+                    })
+
+                }
+            });
 
     }
     vuldDeCombo1();
